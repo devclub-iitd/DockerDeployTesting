32209b563e25946586f5b06673d07b79194e89c4
Jatin Prakash <jatinprakash1511@gmail.com>
2020-05-03 16:14:59 +0530

    add oauth

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const app = express();
 const nodemailer = require("nodemailer");
 const validateEmail = require("./utils")
 const bodyParser = require('body-parser');
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
 
 // Body Parser Middleware
 app.use(bodyParser.json());
@@ -14,12 +16,30 @@ app.get('/', (req, res) => {
 
 app.post('/sendmail', async (req,res) => {
 
-    let transporter = nodemailer.createTransport({
-        service : "Gmail",
-        secure: "true",
+    const oauth2Client = new OAuth2(
+        process.env["CLIENT_ID"], // ClientID
+        process.env["CLIENT_SECRET"], // Client Secret
+        "https://developers.google.com/oauthplayground" // Redirect URL
+    );
+
+    oauth2Client.setCredentials({
+        refresh_token: process.env["REFRESH_TOKEN"]
+    });
+    
+    const accessToken = oauth2Client.getAccessToken()
+    
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        service: 'Gmail',
         auth: {
-          user: process.env["USER_EMAIL"],
-          pass: process.env["USER_PASS"]
+            type: "OAuth2",
+            user: "portfoliocreatoriitd@gmail.com", 
+            clientId: process.env["CLIENT_ID"],
+            clientSecret: process.env["CLIENT_SECRET"],
+            refreshToken: process.env["REFRESH_TOKEN"],
+            accessToken: accessToken
         },
         logger:true,
         debug:true,
@@ -37,7 +57,7 @@ app.post('/sendmail', async (req,res) => {
     try {
         // send mail with defined transport object
         let info = await transporter.sendMail({
-         from: '"Fred Foo 👻" <foo@example.com>', 
+            from: '"Fred Foo 👻" <foo@example.com>', 
             to: email,
             subject: "Hello ✔", 
             text: "Hello world?", 
