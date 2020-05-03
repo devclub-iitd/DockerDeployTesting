2c3df07c802f97f2ba4ad8e8409d056b351ec5d3
Divyansh <divi09112@gmail.com>
2020-05-03 14:20:28 +0530

    add config options

--- src/index.js
+++ src/index.js
@@ -16,10 +16,13 @@ app.post('/sendmail', async (req,res) => {
 
     let transporter = nodemailer.createTransport({
         service : "Gmail",
+        secure: "true",
         auth: {
           user: process.env["USER_EMAIL"],
           pass: process.env["USER_PASS"]
         },
+        logger:true,
+        debug:true,
         proxy : process.env["https_proxy"]
     });
 
@@ -34,11 +37,14 @@ app.post('/sendmail', async (req,res) => {
     try {
         // send mail with defined transport object
         let info = await transporter.sendMail({
-            from: '"Fred Foo 👻" <foo@example.com>', 
+         from: '"Fred Foo 👻" <foo@example.com>', 
             to: email,
             subject: "Hello ✔", 
             text: "Hello world?", 
             html: "<b>Hello world?</b>"
+        }, (err) => {
+            console.log("ERROR");
+            console.log(err)
         });
 
         // dump info about mail to console
