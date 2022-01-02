c9edc4e8a6b36b954992074c8fa35c123b39cef0
Harsh Agrawal <harsh.ag14901@gmail.com>
2022-01-03 04:12:53 +0530

    change

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const OAuth2 = google.auth.OAuth2;
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.send("An alligator approaches!");
+  res.send("An alligator approaches! and it is approaching really fast");
 });
 
 app.get("/healthz", (req, res) => {
